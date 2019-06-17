e42be080e2949807258e309de2846dc3f223021e
AngelicaSantos <angelica.santos2113@gmail.com>
2019-06-17 07:13:53 -0400

    complete

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,21 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+
+//parameterized means you will be adding a type value into the diamond
+// hit control I to bring up pop-up of what you want to implement
+
+public class IdComparator implements Comparator<Item> {
+    public int compare(Item o1, Item o2) {
+        //compare the two id's
+        return Long.compare(o1.getId(),o2.getId());
+    }
+
 }
